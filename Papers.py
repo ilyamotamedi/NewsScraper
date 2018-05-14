965e49cd873e8a2b227dc93fad8f4c0680f2dd23
Ilya Motamedi <ilya.motamedi@gmail.com>
2018-05-14 18:45:19 -0400

    streamlined

--- Papers.py
+++ Papers.py
@@ -1,28 +1,26 @@
 # API for adding, removing, editing papers from JSON data...
 import json
 
-def findPaper(json_file: str, name: str) -> dict:
+def openJSON(json_file):
   with open(json_file) as file:
-    data = json.load(file)
-  return data[name]
+    return json.load(file)
+
+def overWriteJSON(json_file, data):
+  with open(json_file, 'w') as file:
+    json.dump(data, file)
+
+def findPaper(json_file: str, name: str) -> dict:
+  return openJSON(json_file)[name]
 
 def addPaper(json_file: str, name: str, rss: str, link: str) -> None:
-  new_item = {name: {"rss":rss, "link":link}}
-  
-  with open(json_file) as file:
-    data = json.load(file)
-  
-  data.update(new_item)
-  with open(json_file, 'w') as file:
-    json.dump(data, file)
+  data = openJSON(json_file)
+  data.update({name: {"rss":rss, "link":link}})
+  overWriteJSON(json_file, data)
 
 def removePaper(json_file: str, name: str) -> None:
-  with open(json_file) as file:
-    data = json.load(file)
-
+  data = openJSON(json_file)
   del data[name]
-  with open(json_file, 'w') as file:
-    json.dump(data, file)
+  overWriteJSON(json_file, data)
 
 def updatePaper(json_file: str, name: str, rss: str, link: str) -> None:
   removePaper(json_file, name)
